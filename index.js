95ab97f2bd262c1a2a46e687c33d3d58500f4763
dev-93 <cjb940103@gmail.com>
2020-04-05 19:57:50 +0900

    TypeScript to validate that code is working correctly.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log("hello world");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "taenam", age = 28, gender = "male";
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+sayHi(name, age, gender);
 //# sourceMappingURL=index.js.map
